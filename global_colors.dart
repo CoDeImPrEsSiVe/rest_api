591b4d70397a771d9cc5b8f79848d3f1b1c30ba7
Destiny Obasi <104731534+CoDeImPrEsSiVe@users.noreply.github.com>
2024-12-16 19:48:03 +0100

    Add files via upload

---
+++ global_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//Light theme
+Color lightScaffoldColor = const Color(0xFFF8F8F8);
+Color lightCardColor = const Color(0xFFFfFFFf);
+Color lightBackgroundcolor = const Color(0xFFD0E8F2);
+Color lightIconscolor = const Color.fromARGB(255, 175, 0, 120);
+Color lightTextcolor = const Color(0xff324558);
